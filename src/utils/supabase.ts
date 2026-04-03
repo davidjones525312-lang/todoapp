539db62a703683c1fdac71006947e74b1fee16f0
eppeok <47251172+eppeok@users.noreply.github.com>
2026-04-03 20:19:48 +0530

    Fix supabase connection

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = "https://dkhbjjltlaywryjvtmcy.supabase.co";
+const supabaseKey = "sb_publishable_FnIyKIWZtxnZEdCPVMhOYw_h8fvnbRd";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
